ed4a4939389e6a0e622e74eaf2236d383aca2d25
DragosLoL <edrag5@yahoo.com>
2016-01-25 13:23:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "You're Dumb";
 
   options = {
     hostname: 'api.groupme.com',
